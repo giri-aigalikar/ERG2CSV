819aa1598989b68be258d92334523c942c1eda3a
Giri Aigalikar <giri.aigalikar@ipg-automotive.com>
2024-07-17 12:47:14 -0400

    "checking"

--- read_erg_files/Erg2CSVWriter.py
+++ read_erg_files/Erg2CSVWriter.py
@@ -13,7 +13,7 @@ def extract_parameter_names(file_path):
     return parameter_names
 
 # Specify the directory containing the files
-directory_path = os.path.join(os.getcwd(), "example_erg_files")
+directory_path = os.path.join(os.getcwd(), "read_erg_files\\example_erg_files")
 print(directory_path)
 
 # Iterate through all files in the directory
